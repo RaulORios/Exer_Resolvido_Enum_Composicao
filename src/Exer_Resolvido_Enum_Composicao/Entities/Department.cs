66bcc6d9eb9fd1f38c9e7f963b8ef8e7b9c95e90
RaulORios <raul.orios89@outlook.com>
2021-10-05 21:20:23 -0300

    Arquivos adicionados

---
+++ src/Exer_Resolvido_Enum_Composicao/Entities/Department.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Exer_Resolvido_Enum_Composicao.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ src/Exer_Resolvido_Enum_Composicao/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Exer_Resolvido_Enum_Composicao.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
